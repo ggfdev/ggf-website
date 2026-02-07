fa08560f40f85ec1c5f0482e70cf5f4a621f67d5
ggfdev <main@gamegenerationfactory.com>
2026-02-07 23:15:21 +0300

    Refactor Google Tag Manager script integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,18 +30,6 @@ import type { Metadata } from 'next';
   }) {
     return (
       <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
-        <head>
-          {/* Google Tag Manager */}
-          <Script id="gtm-script" strategy="afterInteractive">
-            {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KL84BGLP');
-            `}
-          </Script>
-        </head>
         <body>
           {/* Google Tag Manager (noscript) */}
           <noscript>
@@ -52,6 +40,21 @@ import type { Metadata } from 'next';
               style={{ display: 'none', visibility: 'hidden' }}
             />
           </noscript>
+
+          {/* Google Tag Manager */}
+          <Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-KL84BGLP');
+              `,
+            }}
+          />
 
           <ThemeProvider>
             <LanguageProvider>
