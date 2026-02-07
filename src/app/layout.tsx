d2e6e6759f246427e5b09d6096ceae8993fd6653
ggfdev <main@gamegenerationfactory.com>
2026-02-07 23:58:32 +0300

    Use official Next.js GTM integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-  import type { Metadata } from 'next';
+import type { Metadata } from 'next';
   import { Inter, Space_Grotesk } from 'next/font/google';
-  import Script from 'next/script';
+  import { GoogleTagManager } from '@next/third-parties/google';
   import './globals.css';
   import { LanguageProvider } from '@/LanguageContext';
   import { ThemeProvider } from '@/ThemeContext';
@@ -29,32 +29,8 @@
   }) {
     return (
       <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
-        <head>
-          <Script
-            id="gtm-init"
-            strategy="beforeInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-KL84BGLP');
-              `,
-            }}
-          />
-        </head>
+        <GoogleTagManager gtmId="GTM-KL84BGLP" />
         <body>
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-KL84BGLP"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
-
           <ThemeProvider>
             <LanguageProvider>
               <div className="glow-viewport" />
